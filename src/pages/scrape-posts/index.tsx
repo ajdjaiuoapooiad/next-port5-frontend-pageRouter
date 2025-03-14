6069559cc3202d9042a0d618f40a53d4dbd9c89a
yukito <iyggf66974@yahoo.ne.jp>
2025-03-14 14:39:48 +0900

    scrapePage 追加

---
+++ src/pages/scrape-posts/index.tsx
@@ -0,0 +1,20 @@
+import Sidebar from '@/components/Sidebar'
+import React from 'react'
+
+const ScrapePostsList = () => {
+  return (
+    <div className='grid grid-cols-5'>
+        <div className='col-span-1'> 
+          <Sidebar />
+        </div>
+
+        <div className='col-span-4 p-5'>
+          <div className="grid grid-cols-3">
+            <h1>ScrapeCreatePage</h1>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default ScrapePostsList
