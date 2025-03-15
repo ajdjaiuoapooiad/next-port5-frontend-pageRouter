e157c2de7148b904bce7abfa1838dfa02499cf68
yukito <iyggf66974@yahoo.ne.jp>
2025-03-15 13:32:59 +0900

    Linkタブ　修正

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
@@ -6,14 +7,14 @@ const Navbar = () => {
       <nav className='flex justify-between mx-auto container items-center p-3'>
        
             <div className='text-3xl '>
-              <a href="/posts" className='hover:text-blue-500 transition-all duration-300'>面接管理アプリ</a>
+              <Link href={"/posts"} className='hover:text-blue-500 transition-all duration-300'>面接管理アプリ</Link>
             </div>
 
     
             <div className='space-x-12 font-bold'>
-              <a href="/posts" className='hover:text-blue-500 transition-all duration-300'>ホーム</a>
-              <a href="/create-post" className='hover:text-blue-500 transition-all duration-300'>Create</a>
-              <a href="/posts" className='hover:text-blue-500 transition-all duration-300'>問い合わせ</a>
+              <Link href={"/posts"} className='hover:text-blue-500 transition-all duration-300'>ホーム</Link>
+              <Link href={"/create-post"} className='hover:text-blue-500 transition-all duration-300'>Create</Link>
+              <Link href={"/posts"} className='hover:text-blue-500 transition-all duration-300'>問い合わせ</Link>
             </div>
   
     
