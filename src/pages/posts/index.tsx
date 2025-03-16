e8898861b21959f0db3ab3f54e40f9a46a6390ce
yukito <iyggf66974@yahoo.ne.jp>
2025-03-16 11:25:45 +0900

    error 修正

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -76,7 +76,7 @@ export default function PostsListPage({posts}: Props) {
           <div className="grid grid-cols-3">
             {posts.map((post) => (
            
-              <div  key={post.id} onClick={() => router.push(`posts/${post.id}`)} className="border p-4 my-4 mx-3 col-span-1 hover:shadow-xl bg-gray-100 hover:bg-gray-300 rounded-lg" >
+              <div  key={post.id} onClick={() => router.push(`/posts/${post.id}`)} className="border p-4 my-4 mx-3 col-span-1 hover:shadow-xl bg-gray-100 hover:bg-gray-300 rounded-lg" >
                 <p className="hover:text-blue-500">会社名: {post.company}</p>
                 <p className="hover:text-blue-500">勤務地: {post.place}</p>
                 <p className="hover:text-blue-500">応募状況: {post.status}</p>
