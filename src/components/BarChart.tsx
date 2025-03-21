150e83f6be3c909e6d227f991f058eb0b77db696
yukito <iyggf66974@yahoo.ne.jp>
2025-03-21 11:18:54 +0900

    deploy

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,21 +1,20 @@
 import { Line } from 'react-chartjs-2';
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
-import { chartData2, chartLabels2 } from '../utils/data.json'; // ファイルパスを適切に設定
-
-
+import data from '../utils/data.json'; // ファイルパスを適切に設定
 const BarChart = () => {
   const chartData = {
-    labels: chartLabels2,
+    labels: data.chartLabels2,
     datasets: [
       {
         label: 'エントリー数',
-        data: chartData2,
+        data: data.chartData2,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1,
       },
     ],
   };
+
 
   const options = {
     scales: {
